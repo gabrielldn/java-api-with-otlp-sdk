208c39a6c6b9fb38cc6aea8ed921323080f63643
GabrielLDN <gaellopes@protonmail.com>
2025-03-05 12:29:50 -0300

    initial commit: set up Spring Boot REST API with user management features

---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,40 @@
+package com.example.service;
+
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import com.example.exception.UserNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+            .orElseThrow(() -> new UserNotFoundException("User not found with id " + id));
+    }
+    
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+    
+    public User updateUser(Long id, User userDetails) {
+        User user = getUserById(id);
+        user.setName(userDetails.getName());
+        user.setEmail(userDetails.getEmail());
+        return userRepository.save(user);
+    }
+    
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
